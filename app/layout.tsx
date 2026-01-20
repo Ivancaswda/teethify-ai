7456fac86b460270f9cceb09ccfc112731dbd3bb
aivanius <aivanius@yandex.ru>
2026-01-20 22:46:19 +0700

    track-ai

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,6 +33,13 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+      <head>
+          <script defer
+                  data-website-id="04955ea9-4ded-4ee5-8010-a4dfe4447c35"
+                  data-domain="https://teethify-ai.vercel.app"
+                  src="https://analytity-track.vercel.app/analytics.js">
+          </script>
+      </head>
       <GoogleOAuthProvider clientId={process.env.GOOGLE_CLIENT_ID!} >
           <AuthProvider>
               <Toaster/>
